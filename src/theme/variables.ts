c084b9b04403cae7222732afac3d99757f4d0d7d
Thibault Goehringer <tgoehringer@gmail.com>
2020-08-28 12:13:50 +0200

    Update theme/variables.ts

--- src/theme/variables.ts
+++ src/theme/variables.ts
@@ -1,7 +1,6 @@
 export default {
   customProperties: {
-    '--color-main-1': '#0000FF',
-    '--color-main-2': '#FF0000',
+    '--color-main-1': '#FF0000',
     '--color-black': '#000000',
     '--color-white': '#FFFFFF',
   },
